301b61ea0ab138a7059e6f686760ead9471958c8
Valink16 <valuthyk@gmail.com>
2019-08-21 01:51:50 +0200

    Improved args management and program now waits at the end when launched without arguments

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
+use std::io::{*, Stdout};
+use std::env;
 use nh_fetcher;
 use log;
 use simplelog::*;
 use dialoguer;
 
 fn main() {
-    let fetch_url: String = dialoguer::Input::new()
+    simplelog::TermLogger::init(
+        simplelog::LevelFilter::Info, 
+        simplelog::Config::default(), 
+        simplelog::TerminalMode::default()
+    ).unwrap();
+
+    let args: Vec<String> = env::args().collect();
+    let mut fetch_url: String;
+    let mut link_arg_present: bool;
+    if args.len() < 2 {
+        fetch_url= dialoguer::Input::new()
         .with_prompt("Enter album link").interact()
         .expect("Failed to read user input");
+        link_arg_present = false;
+    } else {
+        fetch_url = args.last().unwrap().to_string();
+        link_arg_present = true;
+    }
 
     let (title, urls) = nh_fetcher::fetch_urls(&fetch_url).unwrap();
     log::info!("Fetching {}", title);
@@ -14,4 +31,10 @@ fn main() {
         .expect("Fetch failure");
         
     log::info!("Successfully downloaded {} out of {} images", success, total);
+    
+    if !link_arg_present {
+        write!(stdout(), "Press any key to continue...").unwrap();
+        stdout().flush().unwrap();
+        stdin().read(&mut [0u8]).unwrap();
+    }
 }
